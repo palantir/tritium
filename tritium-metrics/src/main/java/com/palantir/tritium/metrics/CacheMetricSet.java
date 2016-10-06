bec43dca4e2c73cf3462c6f9cca0831be0fe698d
David Schlosnagle <davids@palantir.com>
2016-10-06 13:23:23 -0400

    Cache metrics gauge snapshot (#44)

--- tritium-metrics/src/main/java/com/palantir/tritium/metrics/CacheMetricSet.java
+++ tritium-metrics/src/main/java/com/palantir/tritium/metrics/CacheMetricSet.java
@@ -18,15 +18,21 @@ package com.palantir.tritium.metrics;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.codahale.metrics.CachedGauge;
+import com.codahale.metrics.DerivativeGauge;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.MetricSet;
+import com.google.common.base.Function;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheStats;
 import com.google.common.collect.ImmutableMap;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+@SuppressFBWarnings({"PT_FINAL_TYPE_PARAM", "PT_FINAL_TYPE_RETURN"})
 class CacheMetricSet implements MetricSet {
 
     private final Cache<?, ?> cache;
@@ -38,65 +44,101 @@ class CacheMetricSet implements MetricSet {
         this.metricsPrefix = metricsPrefix;
     }
 
+    private <T> Gauge<T> derivedGauge(final Function<CacheStats, T> gauge) {
+        // cache the snapshot
+        Gauge<CacheStats> cachedStatsSnapshotGauge = new CachedGauge<CacheStats>(500, TimeUnit.MILLISECONDS) {
+            @Override
+            protected CacheStats loadValue() {
+                return cache.stats();
+            }
+        };
+        return new DerivativeGauge<CacheStats, T>(cachedStatsSnapshotGauge) {
+            @Override
+            protected T transform(CacheStats stats) {
+                return gauge.apply(stats);
+            }
+        };
+    }
+
+    private String cacheMetricName(String... args) {
+        return MetricRegistry.name(metricsPrefix, args);
+    }
+
     @Override
     public Map<String, Metric> getMetrics() {
         ImmutableMap.Builder<String, Metric> cacheMetrics = ImmutableMap.builder();
 
-        cacheMetrics.put(MetricRegistry.name(metricsPrefix, "request", "count"),
-                new Gauge<Long>() {
+        cacheMetrics.put(cacheMetricName("request", "count"),
+                derivedGauge(new Function<CacheStats, Long>() {
                     @Override
-                    public Long getValue() {
-                        return cache.stats().requestCount();
+                    public Long apply(CacheStats stats) {
+                        return stats.requestCount();
                     }
-                });
+                }));
 
-        cacheMetrics.put(MetricRegistry.name(metricsPrefix, "hit", "count"),
-                new Gauge<Long>() {
+        cacheMetrics.put(cacheMetricName("hit", "count"),
+                derivedGauge(new Function<CacheStats, Long>() {
                     @Override
-                    public Long getValue() {
-                        return cache.stats().hitCount();
+                    public Long apply(CacheStats stats) {
+                        return stats.hitCount();
                     }
-                });
-        cacheMetrics.put(MetricRegistry.name(metricsPrefix, "hit", "ratio"),
-                new Gauge<Double>() {
+                }));
+
+        cacheMetrics.put(cacheMetricName("hit", "ratio"),
+                derivedGauge(new Function<CacheStats, Double>() {
                     @Override
-                    public Double getValue() {
-                        CacheStats stats = cache.stats();
+                    public Double apply(CacheStats stats) {
                         return stats.hitCount() / (1.0d * stats.requestCount());
                     }
-                });
+                }));
 
-        cacheMetrics.put(MetricRegistry.name(metricsPrefix, "miss", "count"),
-                new Gauge<Long>() {
+        cacheMetrics.put(cacheMetricName("miss", "count"),
+                derivedGauge(new Function<CacheStats, Long>() {
                     @Override
-                    public Long getValue() {
-                        return cache.stats().missCount();
+                    public Long apply(CacheStats stats) {
+                        return stats.missCount();
                     }
-                });
-        cacheMetrics.put(MetricRegistry.name(metricsPrefix, "miss", "ratio"),
-                new Gauge<Double>() {
+                }));
+
+        cacheMetrics.put(cacheMetricName("miss", "ratio"),
+                derivedGauge(new Function<CacheStats, Double>() {
                     @Override
-                    public Double getValue() {
-                        CacheStats stats = cache.stats();
+                    public Double apply(CacheStats stats) {
                         return stats.missCount() / (1.0d * stats.requestCount());
                     }
-                });
+                }));
 
-        cacheMetrics.put(MetricRegistry.name(metricsPrefix, "eviction", "count"),
-                new Gauge<Long>() {
+        cacheMetrics.put(cacheMetricName("loadSuccess", "count"),
+                derivedGauge(new Function<CacheStats, Long>() {
                     @Override
-                    public Long getValue() {
-                        return cache.stats().evictionCount();
+                    public Long apply(CacheStats stats) {
+                        return stats.loadSuccessCount();
                     }
-                });
+                }));
 
-        cacheMetrics.put(MetricRegistry.name(metricsPrefix, "averageLoadPenalty"),
-                new Gauge<Double>() {
+        cacheMetrics.put(cacheMetricName("loadFailure", "count"),
+                derivedGauge(new Function<CacheStats, Long>() {
                     @Override
-                    public Double getValue() {
-                        return cache.stats().averageLoadPenalty();
+                    public Long apply(CacheStats stats) {
+                        return stats.loadExceptionCount();
                     }
-                });
+                }));
+
+        cacheMetrics.put(cacheMetricName("eviction", "count"),
+                derivedGauge(new Function<CacheStats, Long>() {
+                    @Override
+                    public Long apply(CacheStats stats) {
+                        return stats.evictionCount();
+                    }
+                }));
+
+        cacheMetrics.put(cacheMetricName("averageLoadPenalty"),
+                derivedGauge(new Function<CacheStats, Double>() {
+                    @Override
+                    public Double apply(CacheStats stats) {
+                        return stats.averageLoadPenalty();
+                    }
+                }));
 
         return cacheMetrics.build();
     }
