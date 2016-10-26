9fd0c0c51e1bb8c163ca522a6762a8bb4592776b
David Schlosnagle <davids@palantir.com>
2016-10-26 13:51:41 -0400

    Refactor metric registry reservoir supplier (#55)

---
+++ tritium-metrics/src/main/java/com/palantir/tritium/metrics/HistogramMetricBuilder.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.tritium.metrics;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Reservoir;
+import com.google.common.base.Supplier;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@SuppressFBWarnings("PT_EXTENDS_CONCRETE_TYPE")
+final class HistogramMetricBuilder extends AbstractReservoirMetricBuilder<Histogram> {
+
+    HistogramMetricBuilder(Supplier<Reservoir> reservoirSupplier) {
+        super(Histogram.class, reservoirSupplier);
+    }
+
+    @Override
+    public Histogram newMetric() {
+        final Reservoir reservoir = getReservoirSupplier().get();
+        return new Histogram(reservoir) {
+            @Override
+            public long getCount() {
+                return reservoir.size();
+            }
+        };
+    }
+
+}
