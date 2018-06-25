6a79d11cc62f1efb37929f040839d10e8f0077fb
James Baker <jbaker@palantir.com>
2018-06-25 18:37:35 +0100

    Checkstyle

--- tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/TaggedMetricRegistry.java
+++ tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/TaggedMetricRegistry.java
@@ -22,7 +22,6 @@ import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.Timer;
-import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
 
