69dd320dc5a7535e3b116f3c429c6628a81bfb0d
David Schlosnagle <davids@palantir.com>
2018-10-03 13:44:49 -0400

    Use non-deprecated MockitoJUnitRunner
    Use org.mockito.junit.MockitoJUnitRunner in favor of deprecated
    org.mockito.runners.MockitoJUnitRunner

--- tritium-metrics/src/test/java/com/palantir/tritium/metrics/MetricRegistriesTest.java
+++ tritium-metrics/src/test/java/com/palantir/tritium/metrics/MetricRegistriesTest.java
@@ -44,7 +44,7 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mpierce.metrics.reservoir.hdrhistogram.HdrHistogramReservoir;
 
 @SuppressWarnings("BanGuavaCaches") // this implementation is explicitly for Guava caches
