5ab0956ce72fafa2385693b20fdf0bfd09a31dea
Dan Fox <dfox@palantir.com>
2019-04-26 13:15:40 -0400

    justify 35 seconds

--- tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/SharedTaggedMetricRegistries.java
+++ tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/SharedTaggedMetricRegistries.java
@@ -22,7 +22,16 @@ import java.util.concurrent.TimeUnit;
 /** Analogous to codahale's {@link SharedMetricRegistries}. */
 public final class SharedTaggedMetricRegistries {
 
-    private static final TaggedMetricRegistry DEFAULT = new SlidingWindowTaggedMetricRegistry(1, TimeUnit.MINUTES);
+    /**
+     * With a window size of 35 seconds, metric loggers should dump this registry more frequently (e.g. every 30
+     * seconds). Reading from this registry on a slower interval (e.g. every 5 minutes) is not recommended because
+     * the sample would only represent the last 35seconds, and information from the preceding 4m25 would be lost.
+     *
+     * Increasing the window size would also increase the memory footprint. SlidingTimeWindowArrayReservoir takes
+     * ~128 bits per stored measurement, 10K measurements / sec with reservoir storing time of 35s is:
+     * 10_000 * 35 * 128 / 8 = 5600000 bytes ~ 5 megabytes.
+     */
+    private static final TaggedMetricRegistry DEFAULT = new SlidingWindowTaggedMetricRegistry(35, TimeUnit.SECONDS);
 
     /** Single shared instance for use when it is infeasible to plumb through a user supplied instance. */
     public static TaggedMetricRegistry getSingleton() {
