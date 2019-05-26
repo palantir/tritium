c2b12221812361887c8c35c7424faa7bf1e54e57
David Schlosnagle <davids@palantir.com>
2019-05-26 17:46:24 -0400

    Fix MetricName comparator for tags (#272)
    Ensure `MetricName#safeTags` are compared in a total ordered fashion.

--- tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/MetricName.java
+++ tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/MetricName.java
@@ -17,37 +17,15 @@
 package com.palantir.tritium.metrics.registry;
 
 import com.palantir.logsafe.Safe;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.SortedMap;
 import org.immutables.value.Value;
 
 @Value.Immutable(prehash = true)
 @Value.Style(
-        jdkOnly = true,
         get = {"get*", "is*"},
         overshadowImplementation = true,
         visibility = Value.Style.ImplementationVisibility.PACKAGE)
-public interface MetricName extends Comparable<MetricName> {
-
-    Comparator<Map.Entry<String, String>> entryComparator =
-            Map.Entry.<String, String>comparingByKey()
-                    .thenComparing(Map.Entry.comparingByValue());
-    Comparator<MetricName> metricNameComparator =
-            Comparator.comparing(MetricName::safeName)
-                    .thenComparing(metricName -> metricName.safeTags().entrySet(), (set1, set2) -> {
-                        Iterator<Map.Entry<String, String>> i1 = set1.iterator();
-                        Iterator<Map.Entry<String, String>> i2 = set2.iterator();
-                        while (i1.hasNext() && i2.hasNext()) {
-                            Map.Entry<String, String> e1 = i1.next();
-                            Map.Entry<String, String> e2 = i2.next();
-                            int compare = entryComparator.compare(e1, e2);
-                            if (compare != 0) {
-                                return compare;
-                            }
-                        }
-                        return i1.hasNext() ? -1 : i2.hasNext() ? 1 : 0;
-                    });
+public interface MetricName {
 
     /**
      * General/abstract measure (e.g. server.response-time).
@@ -61,12 +39,8 @@ public interface MetricName extends Comparable<MetricName> {
      * <p>
      * All tags and keys must be {@link Safe} to log.
      */
-    Map<String, String> safeTags();
-
-    @Override
-    default int compareTo(MetricName that) {
-        return metricNameComparator.compare(this, that);
-    }
+    @Value.NaturalOrder
+    SortedMap<String, String> safeTags();
 
     static Builder builder() {
         return new Builder();
