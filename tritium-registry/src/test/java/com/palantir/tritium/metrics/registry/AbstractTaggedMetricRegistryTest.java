5964ce69f2436c5b1687ec34d588a97c0e688116
Dan Sanduleac <dsanduleac@palantir.com>
2019-06-12 18:25:32 +0100

    Wire up metric type into test names, test all types of metric

--- tritium-registry/src/test/java/com/palantir/tritium/metrics/registry/AbstractTaggedMetricRegistryTest.java
+++ tritium-registry/src/test/java/com/palantir/tritium/metrics/registry/AbstractTaggedMetricRegistryTest.java
@@ -22,8 +22,11 @@ import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
+import com.codahale.metrics.Counter;
 import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Meter;
+import com.codahale.metrics.Timer;
 import com.google.common.collect.ImmutableList;
 import com.palantir.tritium.metrics.registry.listeners.TaggedMetricRegistryListener;
 import java.util.function.Consumer;
@@ -40,38 +43,74 @@ import org.mockito.MockitoAnnotations;
 @RunWith(Parameterized.class)
 public class AbstractTaggedMetricRegistryTest {
     @Value.Immutable
-    interface TestCase<T> {
+    interface TestCase {
         Consumer<TaggedMetricRegistry> addMetric();
         Consumer<TaggedMetricRegistryListener> stubOrVerifyMetricAdded();
         Consumer<TaggedMetricRegistryListener> stubOrVerifyMetricRemoved();
-
     }
 
     private static final MetricName NAME = MetricName.builder()
             .safeName("name")
             .build();
 
-    @Parameterized.Parameters
-    public static Iterable<TestCase<?>> data() {
-        Gauge<Integer> gauge = () -> 1;
+    @SuppressWarnings("unchecked")
+    @Parameterized.Parameters(name = "{index} - {0}")
+    public static Iterable<Object[]> data() {
+        Gauge gauge = mock(Gauge.class, "gauge");
         Meter meter = mock(Meter.class, "meter");
+        Histogram histogram = mock(Histogram.class, "histogram");
+        Timer timer = mock(Timer.class, "timer");
+        Counter counter = mock(Counter.class, "counter");
 
         return ImmutableList.of(
-                ImmutableTestCase.<Gauge>builder()
-                        .addMetric(registry -> registry.gauge(NAME, gauge))
-                        .stubOrVerifyMetricAdded(listener -> listener.onGaugeAdded(NAME, gauge))
-                        .stubOrVerifyMetricRemoved(listener -> listener.onGaugeRemoved(NAME))
-                        .build(),
-                ImmutableTestCase.<Gauge>builder()
-                        .addMetric(registry -> registry.meter(NAME, () -> meter))
-                        .stubOrVerifyMetricAdded(listener -> listener.onMeterAdded(NAME, meter))
-                        .stubOrVerifyMetricRemoved(listener -> listener.onMeterRemoved(NAME))
-                        .build()
+                new Object[] {
+                        "gauge",
+                        ImmutableTestCase.builder()
+                                .addMetric(registry -> registry.gauge(NAME, gauge))
+                                .stubOrVerifyMetricAdded(listener -> listener.onGaugeAdded(NAME, gauge))
+                                .stubOrVerifyMetricRemoved(listener -> listener.onGaugeRemoved(NAME))
+                                .build(),
+                },
+                new Object[] {
+                        "meter",
+                        ImmutableTestCase.builder()
+                                .addMetric(registry -> registry.meter(NAME, () -> meter))
+                                .stubOrVerifyMetricAdded(listener -> listener.onMeterAdded(NAME, meter))
+                                .stubOrVerifyMetricRemoved(listener -> listener.onMeterRemoved(NAME))
+                                .build(),
+                },
+                new Object[] {
+                        "histogram",
+                        ImmutableTestCase.builder()
+                                .addMetric(registry -> registry.histogram(NAME, () -> histogram))
+                                .stubOrVerifyMetricAdded(listener -> listener.onHistogramAdded(NAME, histogram))
+                                .stubOrVerifyMetricRemoved(listener -> listener.onHistogramRemoved(NAME))
+                                .build(),
+                },
+                new Object[] {
+                        "timer",
+                        ImmutableTestCase.builder()
+                                .addMetric(registry -> registry.timer(NAME, () -> timer))
+                                .stubOrVerifyMetricAdded(listener -> listener.onTimerAdded(NAME, timer))
+                                .stubOrVerifyMetricRemoved(listener -> listener.onTimerRemoved(NAME))
+                                .build(),
+                },
+                new Object[] {
+                        "counter",
+                        ImmutableTestCase.builder()
+                                .addMetric(registry -> registry.counter(NAME, () -> counter))
+                                .stubOrVerifyMetricAdded(listener -> listener.onCounterAdded(NAME, counter))
+                                .stubOrVerifyMetricRemoved(listener -> listener.onCounterRemoved(NAME))
+                                .build()
+                }
         );
     }
 
-    @Parameterized.Parameter
-    public TestCase<?> testCase;
+    @Parameterized.Parameter(0)
+    public String metricType;
+
+    @Parameterized.Parameter(1)
+    public TestCase testCase;
 
     private final TaggedMetricRegistry registry = new DefaultTaggedMetricRegistry();
 
