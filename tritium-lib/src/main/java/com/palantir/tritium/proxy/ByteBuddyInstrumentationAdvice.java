86f63d78a27636072b583e71415bf8d00557626d
Carter Kozak <ckozak@ckozak.net>
2019-09-03 13:27:18 -0400

    inline advice and make package private

--- tritium-lib/src/main/java/com/palantir/tritium/proxy/ByteBuddyInstrumentationAdvice.java
+++ tritium-lib/src/main/java/com/palantir/tritium/proxy/ByteBuddyInstrumentationAdvice.java
@@ -26,14 +26,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.reflect.Method;
+import java.util.Objects;
 import javax.annotation.Nullable;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.implementation.bytecode.assign.Assigner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** This class must be public to be accessed by generated instrumentation, but is not considered API. */
-public final class ByteBuddyInstrumentationAdvice {
+final class ByteBuddyInstrumentationAdvice {
 
     private static final Logger logger = LoggerFactory.getLogger(InvocationEventProxy.class);
 
@@ -49,8 +49,8 @@ public final class ByteBuddyInstrumentationAdvice {
     @interface MethodIndex {}
 
     @Nullable
-    @Advice.OnMethodEnter(inline = false)
-    public static InvocationContext enter(
+    @Advice.OnMethodEnter
+    static InvocationContext enter(
             @Advice.This Object proxy,
             @Advice.AllArguments Object[] arguments,
             @Advice.FieldValue("instrumentationFilter") InstrumentationFilter filter,
@@ -58,123 +58,47 @@ public final class ByteBuddyInstrumentationAdvice {
             @Advice.FieldValue("methods") Method[] methods,
             @MethodIndex int index) {
         Method method = methods[index];
-        if (isEnabled(proxy, method, arguments, filter, eventHandler)) {
-            return handlePreInvocation(proxy, method, arguments, eventHandler);
+        try {
+            if (eventHandler.isEnabled() && filter.shouldInstrument(proxy, method, arguments)) {
+                return eventHandler.preInvocation(proxy, method, arguments);
+            }
+        } catch (Throwable t) {
+            if (logger.isWarnEnabled()) {
+                logger.warn(
+                        "Failure occurred handling 'preInvocation' invocation on: {}",
+                        UnsafeArg.of("instance", Objects.toString(proxy)),
+                        t);
+            }
         }
         return null;
     }
 
-    @Advice.OnMethodExit(onThrowable = Throwable.class, inline = false, backupArguments = false)
-    public static void exit(
+    @Advice.OnMethodExit(onThrowable = Throwable.class, backupArguments = false)
+    static void exit(
             @Advice.Return(typing = Assigner.Typing.DYNAMIC) Object result,
             @Advice.Thrown Throwable thrown,
             @Advice.FieldValue("invocationEventHandler") InvocationEventHandler<?> eventHandler,
             @Advice.Enter InvocationContext context) {
         if (context != null) {
-            if (thrown == null) {
-                handleOnSuccess(context, result, eventHandler);
-            } else {
-                handleOnFailure(context, thrown, eventHandler);
+            try {
+                if (thrown == null) {
+                    eventHandler.onSuccess(context, result);
+                } else {
+                    eventHandler.onFailure(context, thrown);
+                }
+            } catch (Throwable t) {
+                if (logger.isWarnEnabled()) {
+                    logger.warn(
+                            "Failure occurred handling post-invocation: {}, {}",
+                            UnsafeArg.of("context", context),
+                            safeSimpleClassName("result", thrown == null ? result : thrown),
+                            t);
+                }
             }
         }
     }
 
-    /**
-     * Returns true if instrumentation handling is enabled, otherwise false.
-     *
-     * @return whether instrumentation handling is enabled
-     */
-    private static boolean isEnabled(
-            Object instance,
-            Method method,
-            Object[] args,
-            InstrumentationFilter filter,
-            InvocationEventHandler<?> eventHandler) {
-        try {
-            return eventHandler.isEnabled()
-                    && filter.shouldInstrument(instance, method, args);
-        } catch (Throwable t) {
-            logInvocationWarning("isEnabled", instance, method, t);
-            return false;
-        }
-    }
-
-    @Nullable
-    private static InvocationContext handlePreInvocation(
-            Object instance, Method method, Object[] args, InvocationEventHandler<?> eventHandler) {
-        try {
-            return eventHandler.preInvocation(instance, method, args);
-        } catch (RuntimeException e) {
-            logInvocationWarning("preInvocation", instance, method, e);
-        }
-        return null;
-    }
-
-    private static void handleOnSuccess(
-            @Nullable InvocationContext context, @Nullable Object result, InvocationEventHandler<?> eventHandler) {
-        try {
-            eventHandler.onSuccess(context, result);
-        } catch (RuntimeException e) {
-            logInvocationWarningOnSuccess(context, result, e);
-        }
-    }
-
-    private static void handleOnFailure(
-            @Nullable InvocationContext context, Throwable cause, InvocationEventHandler<?> eventHandler) {
-        try {
-            eventHandler.onFailure(context, cause);
-        } catch (RuntimeException e) {
-            logInvocationWarningOnFailure(context, cause, e);
-        }
-    }
-
-    private static void logInvocationWarningOnSuccess(
-            @Nullable InvocationContext context,
-            @Nullable Object result,
-            Exception cause) {
-        logInvocationWarning("onSuccess", context, result, cause);
-    }
-
-    private static void logInvocationWarningOnFailure(
-            @Nullable InvocationContext context,
-            @Nullable Throwable result,
-            Exception cause) {
-        logInvocationWarning("onFailure", context, result, cause);
-    }
-
-    private static SafeArg<String> safeSimpleClassName(@CompileTimeConstant String name, @Nullable Object object) {
-        return SafeArg.of(name, (object == null) ? "null" : object.getClass().getSimpleName());
-    }
-
-    private static void logInvocationWarning(
-            String event,
-            @Nullable InvocationContext context,
-            @Nullable Object result,
-            Throwable cause) {
-        if (logger.isWarnEnabled()) {
-            logger.warn("{} occurred handling '{}' ({}, {}): {}",
-                    safeSimpleClassName("cause", cause),
-                    SafeArg.of("event", event),
-                    UnsafeArg.of("context", context),
-                    safeSimpleClassName("result", result),
-                    cause);
-        }
-    }
-
-    private static void logInvocationWarning(
-            String event,
-            Object instance,
-            Method method,
-            Throwable cause) {
-        if (logger.isWarnEnabled()) {
-            logger.warn("{} occurred handling '{}' invocation of {} {} on {} instance: {}",
-                    safeSimpleClassName("cause", cause),
-                    SafeArg.of("event", event),
-                    SafeArg.of("class", method.getDeclaringClass().getName()),
-                    SafeArg.of("method", method),
-                    safeSimpleClassName("instanceClass", instance),
-                    UnsafeArg.of("instance", instance),
-                    cause);
-        }
+    static SafeArg<String> safeSimpleClassName(@CompileTimeConstant String name, @Nullable Object object) {
+        return SafeArg.of(name, object == null ? "null" : object.getClass().getSimpleName());
     }
 }
