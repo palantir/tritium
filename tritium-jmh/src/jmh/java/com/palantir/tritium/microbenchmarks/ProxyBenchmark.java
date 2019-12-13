bdfe5c79099a7e0e1a05207863a9f8d56ee9ebab
Carter Kozak <ckozak@ckozak.net>
2019-12-13 14:05:05 -0500

    Benchmark instrumentation implementations for many arguments
    ```
    Benchmark                                        (mode)  Mode  Cnt     Score    Error  Units
    ProxyBenchmark.instrumentedManyArguments     BYTE_BUDDY  avgt    5    60.185 ±  3.184  ns/op
    ProxyBenchmark.instrumentedManyArguments  DYNAMIC_PROXY  avgt    5  5146.058 ± 25.940  ns/op
    ```

--- tritium-jmh/src/jmh/java/com/palantir/tritium/microbenchmarks/ProxyBenchmark.java
+++ tritium-jmh/src/jmh/java/com/palantir/tritium/microbenchmarks/ProxyBenchmark.java
@@ -96,6 +96,7 @@ public class ProxyBenchmark {
     private Service instrumentedDefaultUntagged;
     private Service instrumentedDefaultTagged;
     private Runnable instrumentedFailing;
+    private ManyArguments instrumentedManyArguments;
 
     @Setup
     public void before(Blackhole blackhole) {
@@ -164,6 +165,10 @@ public class ProxyBenchmark {
         instrumentedFailing = Instrumentation.builder(Runnable.class, () -> {
             throw failure;
         })
+                .withPerformanceTraceLogging()
+                .build();
+
+        instrumentedManyArguments = Instrumentation.builder(ManyArguments.class, ManyArguments.Implementation.INSTANCE)
                 .withPerformanceTraceLogging()
                 .build();
 
@@ -251,6 +256,111 @@ public class ProxyBenchmark {
         throw new SafeIllegalStateException("Expected to return");
     }
 
+    @Benchmark
+    public void instrumentedManyArguments() {
+        instrumentedManyArguments.apply(
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+                23,
+                24,
+                25,
+                26,
+                27,
+                28,
+                29,
+                30,
+                31,
+                32,
+                33,
+                34,
+                35,
+                36,
+                37,
+                38,
+                39,
+                40,
+                41,
+                42,
+                43,
+                44,
+                45,
+                46,
+                47,
+                48,
+                49,
+                50,
+                51,
+                52,
+                53,
+                54,
+                55,
+                56,
+                57,
+                58,
+                59,
+                60,
+                61,
+                62,
+                63,
+                64,
+                65,
+                66,
+                67,
+                68,
+                69,
+                70,
+                71,
+                72,
+                73,
+                74,
+                75,
+                76,
+                77,
+                78,
+                79,
+                80,
+                81,
+                82,
+                83,
+                84,
+                85,
+                86,
+                87,
+                88,
+                89,
+                90,
+                91,
+                92,
+                93,
+                94,
+                95,
+                96,
+                97,
+                98,
+                99);
+    }
+
     public interface Service {
         String echo(String input);
     }
@@ -264,7 +374,7 @@ public class ProxyBenchmark {
 
     public static void main(String[] _args) throws Exception {
         Options options = new OptionsBuilder()
-                .include(ProxyBenchmark.class.getSimpleName())
+                .include(ProxyBenchmark.class.getSimpleName() + ".instrumentedManyArguments")
                 .warmupTime(TimeValue.seconds(1))
                 .warmupIterations(5)
                 .measurementTime(TimeValue.seconds(3))
