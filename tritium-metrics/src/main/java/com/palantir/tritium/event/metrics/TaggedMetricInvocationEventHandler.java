92f6af707910254339174e20f250254ca4d5806a
Carter Kozak <ckozak@ckozak.net>
2020-05-21 14:57:58 -0400

    Implement a simple metric-schema based handler
    Changes from the existing tagged metric invocaiton handler:
    * Metrics use constant safe-names.
    * There is no global failure metric, falures are all recorded
      using the standard service-name,method-name tuple.
    * Failures are not tagged with the exception type in order to
      reduce cardinality. Other observability components exist to
      discover failure causes.
    * Metrics are tagged with the provided service name, but no longer
      provide a tag including the declaring class as the two are generally
      either equivalent or interchangable.
    
    These changes are aimed at leveraging auto-generated documentation
    from metric-schema, and allowing general purpose dashboards to take
    advantage of tritium instrumentation metrics.
    
    TODO: Build a reasonable migration path from existing tagged metrics
    to those defined using metric-schema. Ideas?

---
+++ tritium-metrics/src/main/java/com/palantir/tritium/event/metrics/TaggedMetricInvocationEventHandler.java
@@ -0,0 +1,96 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.tritium.event.metrics;
+
+import static com.palantir.logsafe.Preconditions.checkNotNull;
+
+import com.codahale.metrics.Timer;
+import com.palantir.tritium.event.AbstractInvocationEventHandler;
+import com.palantir.tritium.event.DefaultInvocationContext;
+import com.palantir.tritium.event.InstrumentationProperties;
+import com.palantir.tritium.event.InvocationContext;
+import com.palantir.tritium.event.InvocationEventHandler;
+import com.palantir.tritium.metrics.registry.TaggedMetricRegistry;
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BooleanSupplier;
+import java.util.function.Function;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * An implementation of {@link AbstractInvocationEventHandler} whose purpose is to provide tagged metrics as
+ * described in {@code invocation-metrics.yml}.
+ */
+public final class TaggedMetricInvocationEventHandler extends AbstractInvocationEventHandler<InvocationContext> {
+
+    private final String serviceName;
+    private final ConcurrentMap<Method, Timer> timerCache = new ConcurrentHashMap<>();
+    private final Function<Method, Timer> onSuccessTimerMappingFunction;
+    private final InvocationMetrics metrics;
+
+    public static InvocationEventHandler<?> of(TaggedMetricRegistry taggedMetricRegistry, String serviceName) {
+        return new TaggedMetricInvocationEventHandler(taggedMetricRegistry, serviceName);
+    }
+
+    private TaggedMetricInvocationEventHandler(TaggedMetricRegistry taggedMetricRegistry, String serviceName) {
+        super(getEnabledSupplier(serviceName));
+        this.metrics = InvocationMetrics.of(taggedMetricRegistry);
+        this.serviceName = checkNotNull(serviceName, "serviceName");
+        this.onSuccessTimerMappingFunction = method -> metrics.success()
+                .serviceName(serviceName)
+                .methodName(method.getName())
+                .build();
+    }
+
+    @SuppressWarnings("NoFunctionalReturnType") // helper
+    private static BooleanSupplier getEnabledSupplier(String serviceName) {
+        return InstrumentationProperties.getSystemPropertySupplier(serviceName);
+    }
+
+    @Override
+    public InvocationContext preInvocation(@Nonnull Object instance, @Nonnull Method method, @Nonnull Object[] args) {
+        return DefaultInvocationContext.of(instance, method, args);
+    }
+
+    @Override
+    public void onSuccess(@Nullable InvocationContext context, @Nullable Object _result) {
+        debugIfNullContext(context);
+        if (context != null) {
+            long nanos = System.nanoTime() - context.getStartTimeNanos();
+            getSuccessTimer(context.getMethod()).update(nanos, TimeUnit.NANOSECONDS);
+        }
+    }
+
+    private Timer getSuccessTimer(Method method) {
+        return timerCache.computeIfAbsent(method, onSuccessTimerMappingFunction);
+    }
+
+    @Override
+    public void onFailure(@Nullable InvocationContext context, @Nonnull Throwable _cause) {
+        debugIfNullContext(context);
+        if (context != null) {
+            metrics.failure()
+                    .serviceName(serviceName)
+                    .methodName(context.getMethod().getName())
+                    .build()
+                    .mark();
+        }
+    }
+}
---
+++ tritium-metrics/src/test/java/com/palantir/tritium/event/metrics/TaggedMetricInvocationEventHandlerTest.java
@@ -0,0 +1,91 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.tritium.event.metrics;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.codahale.metrics.Metric;
+import com.palantir.logsafe.exceptions.SafeRuntimeException;
+import com.palantir.tritium.event.DefaultInvocationContext;
+import com.palantir.tritium.event.InvocationContext;
+import com.palantir.tritium.event.InvocationEventHandler;
+import com.palantir.tritium.metrics.registry.MetricName;
+import com.palantir.tritium.metrics.registry.TaggedMetricRegistry;
+import com.palantir.tritium.metrics.test.TestTaggedMetricRegistries;
+import java.util.Map;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+final class TaggedMetricInvocationEventHandlerTest {
+
+    public static final class TestImplementation {
+
+        @SuppressWarnings("unused") // instrumented
+        public String doFoo() {
+            return this.getClass().getSimpleName();
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource(TestTaggedMetricRegistries.REGISTRIES)
+    void testTaggedServiceMetricsCaptured(TaggedMetricRegistry registry) throws Exception {
+        TestImplementation testInterface = new TestImplementation();
+
+        InvocationEventHandler<?> handler = TaggedMetricInvocationEventHandler.of(registry, "quux");
+
+        invokeMethod(handler, testInterface, "doFoo", "bar", /* success= */ true);
+
+        Map<MetricName, Metric> metrics = registry.getMetrics();
+        MetricName expectedMetricName = MetricName.builder()
+                .safeName("invocation.success")
+                .putSafeTags("service-name", "quux")
+                .putSafeTags("method-name", "doFoo")
+                .build();
+        assertThat(metrics).containsKey(expectedMetricName);
+    }
+
+    @ParameterizedTest
+    @MethodSource(TestTaggedMetricRegistries.REGISTRIES)
+    void testTaggedServiceMetricsCapturedAsErrors(TaggedMetricRegistry registry) throws Exception {
+        TestImplementation testInterface = new TestImplementation();
+
+        InvocationEventHandler<?> handler = TaggedMetricInvocationEventHandler.of(registry, "quux");
+
+        invokeMethod(handler, testInterface, "doFoo", "bar", /* success= */ false);
+
+        Map<MetricName, Metric> metrics = registry.getMetrics();
+        MetricName expectedMetricName = MetricName.builder()
+                .safeName("invocation.failure")
+                .putSafeTags("service-name", "quux")
+                .putSafeTags("method-name", "doFoo")
+                .build();
+        assertThat(metrics).containsKey(expectedMetricName);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static void invokeMethod(
+            InvocationEventHandler<?> handler, Object obj, String methodName, Object result, boolean success)
+            throws Exception {
+        InvocationContext context =
+                DefaultInvocationContext.of(obj, obj.getClass().getMethod(methodName), null);
+        if (success) {
+            handler.onSuccess(context, result);
+        } else {
+            handler.onFailure(context, new SafeRuntimeException("fail"));
+        }
+    }
+}
