bf3a8e6b2f4d741608b0cfe11bcd7d82b040b36b
Carter Kozak <ckozak@ckozak.net>
2020-07-15 12:36:06 -0400

    BadImport

--- tritium-metrics-jvm/src/test/java/com/palantir/tritium/metrics/jvm/JvmMetricsTest.java
+++ tritium-metrics-jvm/src/test/java/com/palantir/tritium/metrics/jvm/JvmMetricsTest.java
@@ -31,7 +31,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.Entry;
+import java.util.Map;
 import java.util.Objects;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -185,7 +185,7 @@ final class JvmMetricsTest {
                                 .safeTags()
                                 .entrySet()
                                 .containsAll(baseName.safeTags().entrySet()))
-                .map(Entry::getValue)
+                .map(Map.Entry::getValue)
                 .map(type::cast)
                 .collect(MoreCollectors.onlyElement());
     }
