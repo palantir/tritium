2fdd61d09350df12472b4b664f94ced0b8f8c6fd
Carter Kozak <ckozak@ckozak.net>
2020-10-07 11:19:48 -0400

    validate against null entries

--- tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/LockFreeExponentiallyDecayingReservoir.java
+++ tritium-registry/src/main/java/com/palantir/tritium/metrics/registry/LockFreeExponentiallyDecayingReservoir.java
@@ -143,7 +143,7 @@ public final class LockFreeExponentiallyDecayingReservoir implements Reservoir {
             // It's possible that more values were added while the map was scanned, those with the
             // minimum priorities are removed.
             while (newCount > size) {
-                newValues.pollFirstEntry();
+                Preconditions.checkNotNull(newValues.pollFirstEntry(), "Expected an entry");
                 newCount--;
             }
             return new State(alphaNanos, size, newTick, newCount, newValues);
