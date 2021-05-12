df551cdb9ca133746ec77416bc5d8162591491d1
Carter Kozak <ckozak@ckozak.net>
2021-05-12 12:56:12 -0400

    more suppressions?

--- tritium-processor/src/test/java/com/palantir/tritium/processor/TritiumProcessorTest.java
+++ tritium-processor/src/test/java/com/palantir/tritium/processor/TritiumProcessorTest.java
@@ -106,7 +106,7 @@ public final class TritiumProcessorTest {
     }
 
     @Test
-    @SuppressWarnings("deprecation") // testing deprecation linting
+    @SuppressWarnings({"deprecation", "UnnecessarilyFullyQualified"}) // testing deprecation linting
     public void testDeprecatedInterface() {
         assertTestFileCompileAndMatches(TEST_CLASSES_BASE_DIR, com.palantir.tritium.examples.DeprecatedInterface.class);
     }
