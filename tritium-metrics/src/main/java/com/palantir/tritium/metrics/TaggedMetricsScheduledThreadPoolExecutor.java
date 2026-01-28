7e929725fe6720316fb6deec6e83f47208e7eba3
Samuel Souza <ssouza@palantir.com>
2026-01-28 12:04:11 +0000

    docs + checkstyle

--- tritium-metrics/src/main/java/com/palantir/tritium/metrics/TaggedMetricsScheduledThreadPoolExecutor.java
+++ tritium-metrics/src/main/java/com/palantir/tritium/metrics/TaggedMetricsScheduledThreadPoolExecutor.java
@@ -17,12 +17,20 @@
 package com.palantir.tritium.metrics;
 
 import com.codahale.metrics.Timer;
+import com.palantir.tritium.metrics.registry.TaggedMetricRegistry;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Instruments an {@link ScheduledThreadPoolExecutor} that monitors the delay between when a task was scheduled
+ * to be executed and when it actually got executed.
+ * Use in conjunction with {@link MetricRegistries#instrument(TaggedMetricRegistry, ScheduledExecutorService, String)}
+ * to fully instrument an {@link ScheduledExecutorService}.
+ */
 public class TaggedMetricsScheduledThreadPoolExecutor extends ScheduledThreadPoolExecutor {
     private final Timer delay;
 
@@ -33,14 +41,15 @@ public class TaggedMetricsScheduledThreadPoolExecutor extends ScheduledThreadPoo
     }
 
     @Override
-    protected void beforeExecute(Thread t, Runnable r) {
-        if (r instanceof ScheduledFuture<?> sf) {
-            long delay = sf.getDelay(TimeUnit.MILLISECONDS);
-            if (delay < 0) {
-                this.delay.update(-delay, TimeUnit.MILLISECONDS);
+    @SuppressWarnings("checkstyle:DesignForExtension")
+    protected void beforeExecute(Thread thread, Runnable runnable) {
+        if (runnable instanceof ScheduledFuture<?> sf) {
+            long futureDelay = sf.getDelay(TimeUnit.MILLISECONDS);
+            if (futureDelay < 0) {
+                this.delay.update(-futureDelay, TimeUnit.MILLISECONDS);
             }
         }
 
-        super.beforeExecute(t, r);
+        super.beforeExecute(thread, runnable);
     }
 }
